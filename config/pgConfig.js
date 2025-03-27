3693bd855e6f3295384d908966ec7aebd28c0990
nullstead <nullstead@outlook.com>
2025-03-27 14:02:30 +0000

    first publish to ecr

--- config/pgConfig.js
+++ config/pgConfig.js
@@ -4,8 +4,16 @@ dotenv.config();
 
   const { Client } = pg;
 
+  const DBHost = process.env.DBHost;
+  const DBPort = process.env.DBPort;
+  const DBName = process.env.DBName;
+  const DBUsername = process.env.DBUsername;
+  const DBPassword = process.env.DBPassword;
+
+  const DatabaseURL =  `postgres://${DBUsername}:${DBPassword}@${DBHost}:${DBPort}/${DBName}`;
+
   const pgClient = new Client({
-    connectionString: process.env.DatabaseURL
+    connectionString: DatabaseURL
   });
 
   await pgClient.connect();
