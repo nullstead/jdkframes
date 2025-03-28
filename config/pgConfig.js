1d99355e2da4ce5bccf369d1ef0ecc01a9871afb
nullstead <nullstead@outlook.com>
2025-03-28 10:31:28 +0000

    -

--- config/pgConfig.js
+++ config/pgConfig.js
@@ -2,20 +2,26 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
-  const { Client } = pg;
+const { Client } = pg;
 
-  const DBHost = process.env.DBHost;
-  const DBPort = process.env.DBPort;
-  const DBName = process.env.DBName;
-  const DBUsername = process.env.DBUsername;
-  const DBPassword = process.env.DBPassword;
+const DBHost = process.env.DBHost;
+const DBPort = process.env.DBPort;
+const DBName = process.env.DBName;
+const DBUsername = process.env.DBUsername;
+const DBPassword = process.env.DBPassword;
 
-  const DatabaseURL =  `postgres://${DBUsername}:${DBPassword}@${DBHost}:${DBPort}/${DBName}`;
+const DatabaseURL = `postgres://${DBUsername}:${DBPassword}@${DBHost}:${DBPort}/${DBName}`;
 
-  const pgClient = new Client({
+const pgClient = new Client({
     connectionString: DatabaseURL
-  });
+});
 
-  await pgClient.connect();
+try {
+    await pgClient.connect();
+    console.log('Successfully connected to the database! :)');
+} catch (error) {
+    console.error(':( Database connection error! ', error);
+    process.exit(1);
+}
 
-  export default pgClient;
+export default pgClient;
